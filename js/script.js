d840efb896a2e4bf984025dd14137fc1dd24e35a
saezurucrow <usobukuhito@gmail.com>
2019-12-23 13:27:42 +0900

    一旦タイマー諦める

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 $(function () {
+
   $(".start ").on("click", function () {
+
     $(".start").fadeOut();
-    cnt = 5; //5秒前からカウントスタート
-    $('.game').text(cnt);
-    cnDown = setInterval(function () { //1秒おきにカウントマイナス
-      cnt--;
-      if (cnt <= 1) { //0になったら停止する
-        clearInterval(cnDown);
-      }
-      $('.game').text(cnt);
-    }, 1000);
+
+    // カウントダウンタイマー
+    // let cnt = 5; //5秒前からカウントスタート
+
+    // $('.game').text(cnt);
+
+    // cnDown = setInterval(function () { //1秒おきにカウントマイナス
+    //   cnt--;
+
+    //   if (cnt <= 0) { //0になったら停止する
+    //     clearInterval(cnDown);
+    //     game()
+    //   }
+
+    //   $('.game').text(cnt);
+
+    // }, 1000);
+
+
   })
 });
