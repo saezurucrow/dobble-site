205106247cb3c3d9314546e5180e5f3f4657e32e
saezurucrow <usobukuhito@gmail.com>
2019-12-24 11:01:26 +0900

    ランダム画像表示

--- js/script.js
+++ js/script.js
@@ -21,12 +21,37 @@ $(function () {
 
     // }, 1000);
 
+    function shuffle(array) {
+      for (let i = array.length - 1; i >= 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+        // 配列の数値を入れ替える
+        [array[i], array[rand]] = [array[rand], array[i]]
+      }
+      return array;
+    }
+
     // TODO: boardそれぞれに7つのランダムの値をいれ、最後に共通の数字をいれるアルゴリズムをつくる
-    const board = []
-    const board_1 = []
-    const board_2 = []
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
+    array = shuffle(board);
 
+    let board_1 = []
+    let board_2 = []
 
+    for (let j = 0; j <= 6; j++) {
+      board_1.push(board[j])
+      board_2.push(board[j + 7])
+    }
+
+    board_1.push(board[14])
+    board_2.push(board[14])
+
+    board_1 = shuffle(board_1);
+    board_2 = shuffle(board_2);
+
+    for (let k = 0; k <= 7; k++) {
+      $('.board_1').append('<img src="img/' + board_1[k] + '.png"></img>');
+      $('.board_2').append('<img src="img/' + board_2[k] + '.png"></img>');
+    }
   })
 });
