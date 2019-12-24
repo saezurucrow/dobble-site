87403753bf2d5ab303dc2921007aca94cd917559
saezurucrow <usobukuhito@gmail.com>
2019-12-24 15:22:17 +0900

    ゲームを何度もできるようにした

--- js/script.js
+++ js/script.js
@@ -1,62 +1,69 @@
-$(function () {
+same = 0;
 
-  $(".start ").on("click", function () {
+$(".start ").on("click", function () {
 
-    $(".start").fadeOut();
+  // カウントダウンタイマー
+  // let cnt = 5; //5秒前からカウントスタート
 
-    // カウントダウンタイマー
-    // let cnt = 5; //5秒前からカウントスタート
+  // $('.game').text(cnt);
 
-    // $('.game').text(cnt);
+  // cnDown = setInterval(function () { //1秒おきにカウントマイナス
+  //   cnt--;
 
-    // cnDown = setInterval(function () { //1秒おきにカウントマイナス
-    //   cnt--;
+  //   if (cnt <= 0) { //0になったら停止する
+  //     clearInterval(cnDown);
+  //     game()
+  //   }
 
-    //   if (cnt <= 0) { //0になったら停止する
-    //     clearInterval(cnDown);
-    //     game()
-    //   }
+  //   $('.game').text(cnt);
 
-    //   $('.game').text(cnt);
+  // }, 1000);
 
-    // }, 1000);
-
-    function shuffle(array) {
-      for (let i = array.length - 1; i >= 0; i--) {
-        let rand = Math.floor(Math.random() * (i + 1));
-        // 配列の数値を入れ替える
-        [array[i], array[rand]] = [array[rand], array[i]]
-      }
-      return array;
+  function shuffle(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      // 配列の数値を入れ替える
+      [array[i], array[rand]] = [array[rand], array[i]]
     }
+    return array;
+  }
 
-    function array_set() {
-      const board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-      array = shuffle(board);
-      let board_1 = []
-      let board_2 = []
-      for (let j = 0; j <= 6; j++) {
-        board_1.push(array[j])
-        board_2.push(array[j + 7])
-      }
-      board_1.push(board[14])
-      board_2.push(board[14])
-      board_1 = shuffle(board_1);
-      board_2 = shuffle(board_2);
-      return {
-        board_1,
-        board_2
-      }
+  function array_set() {
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    array = shuffle(board);
+    let board_1 = []
+    let board_2 = []
+    for (let j = 0; j <= 6; j++) {
+      board_1.push(array[j])
+      board_2.push(array[j + 7])
     }
-
-    let {
+    same = board[14]
+    board_1.push(same)
+    board_2.push(same)
+    board_1 = shuffle(board_1);
+    board_2 = shuffle(board_2);
+    return {
       board_1,
       board_2
-    } = array_set();
-
-    for (let k = 0; k <= 7; k++) {
-      $('.board_1').append('<img class="img_' + board_1[k] + '" src="img/' + board_1[k] + '.png"></img>');
-      $('.board_2').append('<img class="img_' + board_2[k] + '" src="img/' + board_2[k] + '.png"></img>');
     }
-  })
+  }
+
+  let {
+    board_1,
+    board_2
+  } = array_set();
+
+  for (let k = 0; k <= 7; k++) {
+    $('.board_1').append('<img class="img_' + board_1[k] + '" src="img/' + board_1[k] + '.png"></img>');
+    $('.board_2').append('<img class="img_' + board_2[k] + '" src="img/' + board_2[k] + '.png"></img>');
+  }
+
+  $(".img_" + same).on("click", function () {
+    console.log("おめでとう");
+    for (let k = 0; k <= 7; k++) {
+      $('.board_1').empty();
+      $('.board_2').empty();
+    }
+    $('.start').text("リセット");
+  });
 });
